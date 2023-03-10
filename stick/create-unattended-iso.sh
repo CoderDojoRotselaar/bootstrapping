012101bed57b0e8d0a95f4ba95ef7d6e58751c23
Jo Vandeginste <Jo.Vandeginste@kuleuven.be>
2023-03-10 21:26:23 +0100

    Update for Ubuntu 22.04
    Signed-off-by: Jo Vandeginste <Jo.Vandeginste@kuleuven.be>

--- stick/create-unattended-iso.sh
+++ stick/create-unattended-iso.sh
@@ -1,20 +1,12 @@
 #!/bin/bash -eu
 
-# Needed tools: 7z, xorriso, isolinux
+# Needed tools: 7z, xorriso
 
-ISO="isos/ubuntu-22.04.1-desktop-amd64.iso"
+ISO="isos/ubuntu-22.04.2-live-server-amd64.iso"
+# ISO="isos/ubuntu-20.04.5-desktop-amd64.iso"
 
 if [[ ! -f "${ISO}" ]]; then
-  echo "You should download the Ubuntu 20.04.3 live server iso to '${ISO}'"
-  exit 1
-fi
-
-for file in /usr/lib/ISOLINUX/isohdpfx.bin /usr/share/syslinux/isohdpfx.bin; do
-  [[ -f "${file}" ]] && ISOLINUX="${file}"
-done
-
-if [[ -z "${ISOLINUX}" ]]; then
-  echo "Could not find isohdpfx.bin; you may need to install the isolinux package..."
+  echo "You should download the Ubuntu live server iso to '${ISO}'"
   exit 1
 fi
 
@@ -28,8 +20,11 @@ if [[ "${SCRATCH:-}" == "1" ]]; then
 
   # Update boot flags with cloud-init autoinstall:
   ## Should look similar to this: initrd=/casper/initrd quiet autoinstall ds=nocloud;s=/cdrom/nocloud/ ---
+  set +e
   sed -i 's|---|autoinstall ds=nocloud\\\;s=/cdrom/nocloud/ ---|g' iso/boot/grub/grub.cfg
+  sed -i 's|---|autoinstall ds=nocloud\\\;s=/cdrom/nocloud/ ---|g' iso/boot/grub/loopback.cfg
   sed -i 's|---|autoinstall ds=nocloud;s=/cdrom/nocloud/ ---|g' iso/isolinux/txt.cfg
+  set -e
 
   # Disable mandatory md5 checksum on boot:
   md5sum iso/.disk/info >iso/md5sum.txt
@@ -38,12 +33,23 @@ fi
 
 rsync -av --delete nocloud/ iso/nocloud/
 
+7z -y x "${ISO}" '[BOOT]'
+
+rm -v -f coderdojo-autoinstall.iso
+
 xorriso -as mkisofs -r \
-  -V "CoderDojo automated installation" \
+  -V 'CoderDojo automated installation' \
   -o coderdojo-autoinstall.iso \
-  -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot \
-  -boot-load-size 4 -boot-info-table \
-  -eltorito-alt-boot -e boot/grub/efi.img -no-emul-boot \
-  -isohybrid-gpt-basdat -isohybrid-apm-hfsplus \
-  -isohybrid-mbr "${ISOLINUX}" \
-  iso/boot iso
+  --grub2-mbr "./[BOOT]/1-Boot-NoEmul.img" \
+  -partition_offset 16 \
+  --mbr-force-bootable \
+  -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b "./[BOOT]/2-Boot-NoEmul.img" \
+  -appended_part_as_gpt \
+  -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 \
+  -c '/boot.catalog' \
+  -b '/boot/grub/i386-pc/eltorito.img' \
+  -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info \
+  -eltorito-alt-boot \
+  -e '--interval:appended_partition_2:::' \
+  -no-emul-boot \
+  iso
